6ef86a081fe67de6fcac678565e54a32e7fadc9e
Andrej Pedchenko <graduadu@gmail.com>
2022-05-08 17:42:28 +0300

    Created guide view

---
+++ Honeymoon/View/GuideView.swift
@@ -0,0 +1,77 @@
+import SwiftUI
+
+struct GuideView: View {
+    // MARK: - PROPERTY
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                HeaderComponent()
+                
+                Spacer(minLength: 10)
+                
+                Text("Get Started!")
+                    .fontWeight(.black)
+                    .font(.largeTitle)
+                    .foregroundColor(Color.pink)
+                
+                Text("Discover and pick the perfect destination for your romantic Honeymoon!")
+                    .lineLimit(nil)
+                    .multilineTextAlignment(.center)
+                
+                Spacer(minLength: 10)
+                
+                VStack(alignment: .leading, spacing: 25) {
+                    GuideComponent(
+                        title: "Like",
+                        subtitle: "Swipe right",
+                        description: "Do you like this detination? Touch the screen and swipe right . It will be saved to the favourites.",
+                        icon: "heart.circle")
+                    
+                    GuideComponent(
+                        title: "Dismiss",
+                        subtitle: "Swipe left",
+                        description: "Would you rather skip this place? Touch the screen and swipe left. You will no longer see it.",
+                        icon: "xmark.circle"
+                    )
+                    
+                    GuideComponent(
+                        title: "Book",
+                        subtitle: "Tap the button",
+                        description: "Our selection of honeymoon resorts is perfect settings for you to embark your new life together.",
+                        icon: "checkmark.square"
+                    )
+                } //: VSTACK
+                
+                Spacer(minLength: 10)
+                
+                Button(action: {
+                    // ACTION
+                    print("A button was tapped")
+                }) {
+                    Text("Continue".uppercased())
+                        .font(.headline)
+                        .padding()
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .background(Capsule().fill(Color.pink))
+                        .foregroundColor(Color.white)
+                } //: BUTTON
+            } //: VSTACK
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding(.top, 15)
+            .padding(.bottom, 25)
+            .padding(.horizontal, 25)
+        } //: SCROLL
+    }
+}
+
+// MARK: - PREVIEW
+
+struct GuideView_Previews: PreviewProvider {
+    static var previews: some View {
+        GuideView()
+            .previewDevice("iPhone 13 Pro")
+    }
+}
