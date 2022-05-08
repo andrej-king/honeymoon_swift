c9bf0bc84a995ad8f2f4190acde74a4357f5e914
Andrej Pedchenko <graduadu@gmail.com>
2022-05-08 18:48:56 +0300

    Created subview for Credits and added information

--- Honeymoon/View/InfoView.swift
+++ Honeymoon/View/InfoView.swift
@@ -22,11 +22,7 @@ struct InfoView: View {
                     .fontWeight(.black)
                     .modifier(TitleModifier())
                 
-                HStack {
-                    Text("Photos").foregroundColor(Color.gray)
-                    Spacer()
-                    Text("Unsplash")
-                } //: HSTACK
+                CreditsView()
                 
                 Spacer(minLength: 10)
                 
@@ -83,7 +79,29 @@ struct RowAppInfoView: View {
                 Spacer()
                 Text(itemTwo)
             } //: HSTACK
+            
+            Divider()
         } //: VSTACK
-        Divider()
+    }
+}
+
+struct CreditsView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            HStack {
+                Text("Photos").foregroundColor(Color.gray)
+                Spacer()
+                Text("Unsplash")
+            } //: HSTACK
+        
+            Divider()
+            
+            Text("Photographers")
+                .foregroundColor(Color.gray)
+            
+            Text("Shifaaz Shamoon (Maldives), Grillot Edouard (France), Evan Wise (Greece), Christoph Schulz (United Arab Emirates), Andrew Coelho (USA), Damiano Baschiera (Italy), Daniel Olah (Hungary), Andrzej Rusinowski (Poland), Lucas Miguel (Slovenia), Florencia Potter (Spain), Ian Simmonds (USA), Ian Keefe (Canada), Denys Nevozhai (Thailand), David Köhler (Italy), Andre Benz (USA), Alexandre Chambon (South Korea), Roberto Nickson (Mexico), Ajit Paul Abraham (UK), Jeremy Bishop (USA), Davi Costa (Brazil), Liam Pozz (Australia)")
+                .multilineTextAlignment(.leading)
+                .font(.footnote)
+        } //: VSTACK
     }
 }
