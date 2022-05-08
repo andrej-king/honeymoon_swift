bab82c240d8989f04075c58e3df8fbb162e415db
Andrej Pedchenko <graduadu@gmail.com>
2022-05-08 21:01:25 +0300

    Added gesture with LongPressGesture and hide header, footer.

--- Honeymoon/View/ContentView.swift
+++ Honeymoon/View/ContentView.swift
@@ -6,6 +6,7 @@ struct ContentView: View {
     @State var showAlert: Bool = false
     @State var showGuide: Bool = false
     @State var showInfo: Bool = false
+    @GestureState private var dragState = DragState.inactive
     
     // MARK: - CARD VIEWS
     
@@ -27,12 +28,49 @@ struct ContentView: View {
         return index == 0
     }
     
+    // MARK: - DRAG STATES
+    
+    enum DragState {
+        case inactive
+        case pressing
+        case dragging(translation: CGSize)
+        
+        var translation: CGSize {
+            switch self {
+            case .inactive, .pressing:
+                return .zero
+            case .dragging(let translation):
+                return translation
+            }
+        }
+        
+        var isDragging: Bool {
+            switch self {
+            case .dragging:
+                return true
+            case .pressing, .inactive:
+                return false
+            }
+        }
+        
+        var isPressing: Bool {
+            switch self {
+            case .pressing, .dragging:
+                return true
+            case .inactive:
+                return false
+            }
+        }
+    }
+    
     // MARK: - BODY
     
     var body: some View {
         VStack {
             // MARK: - HEADER
             HeaderView(showGuideView: $showGuide, showInfoView: $showInfo)
+                .opacity(dragState.isDragging ? 0.0 : 1.0)
+                .animation(.default, value: dragState.isDragging)
             
             Spacer()
             
@@ -41,17 +79,29 @@ struct ContentView: View {
                 ForEach(cardViews) { cardView in
                     cardView
                         .zIndex(self.isTopCard(cardView: cardView) ? 1 : 0)
+                        .gesture(LongPressGesture(minimumDuration: 0.01)
+                            .sequenced(before: DragGesture())
+                            .updating(self.$dragState, body: { (value, state, transaction) in
+                                switch value {
+                                case .first(true):
+                                    state = .pressing
+                                case .second(true, let drag):
+                                    state = .dragging(translation: drag?.translation ?? .zero)
+                                default:
+                                    break
+                                }
+                            })
+                        )
                 }
             } //: ZSTACK
             .padding(.horizontal)
-//            CardView(honeymoon: honeymoonData[2])
-//            // FIXME: Add padding to the cards later on.
-//                .padding()
             
             Spacer()
             
             // MARK: - FOOTER
             FooterView(showBookingAlert: $showAlert)
+                .opacity(dragState.isDragging ? 0.0 : 1.0)
+                .animation(.default, value: dragState.isDragging)
         } //: VSTACK
         .alert(isPresented: $showAlert) {
             Alert(
